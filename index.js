51e50fc3127627df7d695408fdb2885684dfd67a
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-04 20:44:14 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    let paragraph = this.getElementById('text');
+    paragraph.textContent = "This is really cool!";
+  });
